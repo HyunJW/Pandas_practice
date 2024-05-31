57c40bb74e626ae55bb5f4088945a51994847861
HyunJW <hyunjw0511@naver.com>
2024-05-31 14:17:52 +0900

    Day 4 of studying Java(change ex4_20)

--- Java/Ch04/src/Ex4_20.java
+++ Java/Ch04/src/Ex4_20.java
@@ -8,7 +8,7 @@ public class Ex4_20 {
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		outer:
+		outer: // while문 이름 부여
 		while (true) {
 			System.out.println("(1) square");
 			System.out.println("(2) square root");
@@ -28,8 +28,27 @@ public class Ex4_20 {
 			
 			for (;;) {
 				System.out.print("계산할 값을 입력하세요.(계산 종료:0, 전체 종료:99)>");
+				tmp = scanner.nextLine();
+				num = Integer.parseInt(tmp);
+				
+				if (num == 0)
+					break; // 계산 종료
+				
+				if (num == 99)
+					break outer; // 전체 종료
+				
+				switch (menu) {
+				case 1:
+					System.out.println("result="+ num*num);
+					break;
+				case 2:
+					System.out.println("result="+ Math.sqrt(num));
+					break;
+				case 3:
+					System.out.println("result="+ Math.log(num));
+					break;
+				}		
 			}
-			
 		}
 		scanner.close();
 	}
