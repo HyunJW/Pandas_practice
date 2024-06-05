380de373e55776fd1e0d1119e8741378a74f2f65
HyunJW <hyunjw0511@naver.com>
2024-06-05 11:48:42 +0900

    Day 9 of studying Java(Exception example)

---
+++ Java/Ch08/src/Ex8_1.java
@@ -0,0 +1,14 @@
+
+public class Ex8_1 {
+
+	public static void main(String[] args) {
+		System.out.println(1);
+		try {
+			System.out.println(2);
+			System.out.println(3);
+		} catch (Exception e) {
+			System.out.println(4); // ½ÇÇà x
+		}
+		System.out.println(5);
+	}
+}
---
+++ Java/Ch08/src/Ex8_2.java
@@ -0,0 +1,14 @@
+
+public class Ex8_2 {
+
+	public static void main(String[] args) {
+		System.out.println(1);
+		try {
+			System.out.println(0/0); // error 발생
+			System.out.println(2); // 실행 x
+		} catch (ArithmeticException ae) {
+			System.out.println(3);
+		}
+		System.out.println(4);
+	}
+}
