c2abc2e2fc14b0f302d3537e3de86d2a48d74d1e
Jungwhan Hyun <hyunjw0511@naver.com>
2024-06-19 17:20:31 +0900

    Day 18 of studying Java(I/O example)

---
+++ Java/Ch15/src/Ex15_13.java
@@ -0,0 +1,8 @@
+
+public class Ex15_13 {
+	public static void main(String[] args) {
+		String hello = "Hello World!";
+		System.out.println("out: " + hello);
+		System.err.println("err: " + hello);
+	}
+}
