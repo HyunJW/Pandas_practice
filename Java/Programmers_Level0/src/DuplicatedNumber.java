c7af8c55dd99c7168d734817f03a418d15be0cd1
Jungwhan Hyun <hyunjw0511@naver.com>
2024-06-22 00:00:34 +0900

    Day 20 of studying Java(Programmers coddingtest level0)

--- Java/Programmers_Level0/src/DuplicatedNumber.java
+++ Java/Programmers_Level0/src/DuplicatedNumber.java
@@ -1,9 +1,14 @@
+import java.util.Arrays;
 
 public class DuplicatedNumber {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static int solution(int[] array, int n) {
+		return (int)Arrays.stream(array).filter(num -> num==n).count();
 	}
-
+	
+	public static void main(String[] args) {
+		int[] intArr1 = {1, 1, 2, 3, 4, 5};
+		int[] intArr2 = {0, 2, 3, 4};
+		System.out.println(solution(intArr1, 1));
+		System.out.println(solution(intArr2, 1));
+	}
 }
